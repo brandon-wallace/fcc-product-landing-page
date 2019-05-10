4aa8b8c1b0fa11585c7e53ee8a3e04046f66c9ec
bwallace <17948072+brandon-wallace@users.noreply.github.com>
2019-05-09 23:34:22 -0400

    Adding modal windows
    Adding modal windows to display each image. Work in progress.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+// const tileImage = document.querySelectorAll('.time-img')[0];
+// const close = document.querySelector('.modal-close')[0];
+
+const openModal = () => {
+    console.log(`Opening modal`)
+    document.querySelector('.modal-content').style.display = 'flex';
+}
+
+// tileImage.addEventListener('click', () => {
+//   document.querySelector('.modal-content').style.display = 'flex';
+// })
+
+
+// close.addEventListener('click').style.display = none;
