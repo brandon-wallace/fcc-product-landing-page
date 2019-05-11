602a21ac718e74126c29d106aa3548698bbbf425
bwallace <17948072+brandon-wallace@users.noreply.github.com>
2019-05-11 17:51:02 -0400

    Click to open
    Use Javascript to open images by adding an event listener to each
    tile. Looping through the node list of the tiles an image opens
    by setting display flex on the modal window.
    
    Another onclick function closes the modal window.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 "use strict";
 
 
-// const tileImage = document.querySelectorAll('.time-img')[0];
-// const close = document.querySelector('.modal-close')[0];
+const tileImage = document.querySelectorAll('.tile-img');
+
 
 const openModal = () => {
-    console.log(`Opening modal`)
-    document.querySelector('.modal-content').style.display = 'flex';
+    console.log(`Opening modal`);
+    document.querySelector('.modal').style.display = 'flex';
 }
 
-// tileImage.addEventListener('click', () => {
-//   document.querySelector('.modal-content').style.display = 'flex';
-// })
+for (let i = 0; i < tileImage.length; i++) {
+    tileImage[i].addEventListener('click', () => {
+        openModal();
+    });
+}
 
 
-// close.addEventListener('click').style.display = none;
+const closeModal = () => {
+    console.log(`Closing modal`);
+    document.querySelector('.modal').style.display = 'none';
+}
